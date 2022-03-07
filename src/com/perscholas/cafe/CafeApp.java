233092ff984dc2d38ef9160846ab949da3c957d0
Joshua_Johnson <joshualj2018@gmail.com>
2022-03-06 20:28:25 -0600

    "Cafe and Product, Project 1"

---
+++ src/com/perscholas/cafe/CafeApp.java
@@ -0,0 +1,39 @@
+package com.perscholas.cafe;
+
+import com.perscholas.cafe.Product;
+
+import java.util.Scanner;
+
+public class CafeApp {
+
+    public static void main(String[] args) {
+        Product coffee = new Product("coffee", 5.00, "regular coffee");
+        Product espresso = new Product("espresso", 4.00, "nice caffeine kick");
+        Product cappuccino = new Product("cappuccino", 3.50, "a cap for a captain.");
+
+        Scanner productQuant = new Scanner(System.in);
+        System.out.println("Please enter a quantity of coffees, espressos, and cappuccinos in the format 1 1 1: ");
+        String[] productQuants = productQuant.nextLine().split(" ");
+
+        int coffeeQuant = Integer.parseInt(productQuants[0]);
+        int espressoQuant = Integer.parseInt(productQuants[1]);
+        int cappuccinoQuant = Integer.parseInt(productQuants[2]);
+
+        coffee.setQuantity(coffeeQuant);
+        espresso.setQuantity(espressoQuant);
+        cappuccino.setQuantity(cappuccinoQuant);
+
+        double coffeeSubtotal = coffee.calculateProductTotal();
+        double espressoSubtotal = espresso.calculateProductTotal();
+        double cappuccinoSubtotal = cappuccino.calculateProductTotal();
+
+        System.out.printf("The subtotal of your %s, with description of '%s', is: %.2f\n", coffee.getName(), coffee.getDescription(), coffeeSubtotal);
+        System.out.printf("The subtotal of your %s, with description of '%s', is: %.2f\n", espresso.getName(), espresso.getDescription(), espressoSubtotal);
+        System.out.printf("The subtotal of your %s, with description of '%s', is: %.2f\n", cappuccino.getName(), cappuccino.getDescription(), cappuccinoSubtotal);
+
+        double salesTax = 0.6875;
+        double subTotal = cappuccinoSubtotal + espressoSubtotal + coffeeSubtotal;
+        double salesTotal = subTotal + (salesTax * subTotal);
+        System.out.printf("Sales subtotal: %.2f\nSales Tax: %.2f\nSales Total: %.2f", subTotal, salesTax, salesTotal);
+    }
+}
---
+++ src/com/perscholas/cafe/Product.java
@@ -0,0 +1,61 @@
+package com.perscholas.cafe;
+
+public class Product {
+    private String name;
+    private double price;
+    private String description;
+    private int quantity;
+
+    public Product(String name, double price, String description, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+    public Product(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    private Product() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double calculateProductTotal() {
+        return (quantity * price);
+    }
+
+}
